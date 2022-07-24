2c7e8a5e3f4e6886c469ad4cea1e0595faf0cdd0
Mirkhat Asen <asen.mirkhat@bk.ru>
2022-07-24 17:56:01 +0600

    publisher entity created

---
+++ src/main/java/com/epam/bookstore/entity/Publisher.java
@@ -0,0 +1,39 @@
+package com.epam.bookstore.entity;
+
+public class Publisher {
+
+    private Long id;
+    private String name;
+
+    public Publisher() {
+    }
+
+    public Publisher(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Publisher{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
